b0e4e3e29feae2ce399dd106cf108837167805a3
snikdab dabaro <proteopro@gmail.com>
2023-11-19 17:58:11 +0200

    cbc random access

--- AES/AES_128_CBC.py
+++ AES/AES_128_CBC.py
@@ -3,6 +3,10 @@ from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
 import binascii
 import os
 
+def pad_text(text):
+    text = text + ((16 - len(text) %16) * b'\0')
+    return text
+
 def byte_xor(ba1, ba2):
     return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
 
@@ -19,23 +23,23 @@ def AES_128_CBC_encrypt(plaintext, key, iv):
     encryptor = cipher.encryptor()
 
     #pad plaintext to be a multiple of 16 bytes
-    plaintext = plaintext + ((16 - len(plaintext) %16) * b'\0')
+    plaintext = pad_text(plaintext)
 
     #cut plaintext into 16 byte blocks and loop through them
-    print("-------------------------------------------------------------")
-    print("Processing Block:", plaintext[0:16])
+    print("--------------------------Encrypt----------------------------")
+    print("Processing Block:", plaintext[0:16].hex())
     xor_block = byte_xor(iv, plaintext[0:16])
     ct = encryptor.update(xor_block)
     blockholder = ct
-    print("Cipher Text:", ct)
+    print("Cipher Text:", ct.hex())
     print("-------------------------------------------------------------")
 
     for i in range(16, len(plaintext), 16):
-        print("Processing Block:", plaintext[i:i+16])
+        print("Processing Block:", plaintext[i:i+16].hex())
         xor_block = byte_xor(ct, plaintext[i:i+16])
         ct = encryptor.update(xor_block)
         blockholder += ct
-        print("Cipher Text:", ct)
+        print("Cipher Text:", ct.hex())
         print("-------------------------------------------------------------")
 
     return blockholder
@@ -46,20 +50,20 @@ def AES_128_CBC_decrypt(ciphertext, key, iv):
     depcryptor = cipher.decryptor()
 
     #cut plaintext into 16 byte blocks and loop through them
-    print("-------------------------------------------------------------")
-    print("Processing Block:", ciphertext[0:16])
+    print("----------------------Decrypt---------------------------------")
+    print("Processing Block:", ciphertext[0:16].hex())
     pt = depcryptor.update( ciphertext[0:16])
     xor_block = byte_xor(iv, pt[0:16])
     blockholder = xor_block
-    print("Plain Text:", xor_block)
-    print("-------------------------------------------------------------")
+    print("Plain Text:", xor_block.hex())
+    print("--------------------------------------------------------------")
 
     for i in range(16, len(ciphertext), 16):
-        print("Processing Block:", ciphertext[i:i+16])
+        print("Processing Block:", ciphertext[i:i+16].hex())
         pt = depcryptor.update(ciphertext[i:i+16])
         xor_block = byte_xor(pt, ciphertext[i-16:i])
         blockholder += xor_block
-        print("Plain Text:", xor_block)
+        print("Plain Text:", xor_block.hex())
         print("-------------------------------------------------------------")
 
     return blockholder
@@ -69,12 +73,21 @@ def test(text, key, iv):
     pad_len = (16 - len(text) %16)
     ct=AES_128_CBC_encrypt(text, key, iv)
     pt=AES_128_CBC_decrypt(ct, key, iv)
-    print("=====================================================================")
+    print("\n============== TEST1 FEEDING RESULT OF ENCRYPTION TO DECRYTPION =================")
     print("Original Plaintext   :", text.hex())
     print("Ciphertext           :", ct.hex())
     pt = pt[:-pad_len] #remove pad from plaintext
     print("Decrypted Ciphertext :", pt.hex()) 
-    print("=====================================================================")
+    print("============================== END OF TEST 1 ====================================\n")
+    print("\n================== TEST2 COMPARE WITH MODULES AES 128 CBC =====================")
+    cipher = Cipher(algorithms.AES128(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    text = pad_text(text)
+    ct2 = encryptor.update(text) + encryptor.finalize()
+    print("Original Plaintext             : ", text.hex())
+    print("my cipher text encryption      : ", ct.hex())
+    print("Cryptography module encryption : ", ct2.hex())
+    print("============================== END OF TEST 2 ====================================\n")
     return 
 
 
@@ -95,12 +108,16 @@ def main():
         raise argparse.ArgumentTypeError(f"Invalid key length: {len(key)}")
     if len(iv) != 32:
         raise argparse.ArgumentTypeError(f"Invalid iv length: {len(iv)}")
+    if len(plaintext) == 0:
+        raise argparse.ArgumentTypeError(f"Invalid plaintext length: {len(plaintext)}")
 
     bplaintext = parse_hexadecimal(plaintext)
     bkey = parse_hexadecimal(key)
     biv = parse_hexadecimal(iv)
 
     test(bplaintext, bkey, biv)
+    return
 
+# a good run test to check python3 AES_128_random_acces.py -p ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa11111111111111111111111111 -k 199494cdea9c646e76015c5bd3ffdaec -iv 199494cdea9c646e76015c5bd3ffdaec
 if __name__ == "__main__":
     main()
---
+++ AES/AES_128_random_acces.py
@@ -0,0 +1,64 @@
+import argparse
+from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
+from AES_128_CBC import parse_hexadecimal, AES_128_CBC_encrypt, byte_xor
+
+def AES_random_access(ciphertext, key, iv, index):
+    #assert index >= 0
+    if index < 0:
+        raise argparse.ArgumentTypeError(f"Invalid index: {index}")
+    # Create an AES-128-ECB cipher object
+    cipher = Cipher(algorithms.AES128(key), modes.ECB())
+    depcryptor = cipher.decryptor()
+    i = index * 16
+    print("Random Access for Block:", ciphertext[i:i+16].hex(), "at index:", index)
+    pt = depcryptor.update(ciphertext[i:i+16])
+    if i == 0:
+        print("Using IV for XOR:", iv.hex())
+        xor_block = byte_xor(iv, pt[0:16])
+    else:
+        print("Using previous block for XOR:", ciphertext[i-16:i].hex(), "at index:", index-1)
+        xor_block = byte_xor(pt, ciphertext[i-16:i])
+    print("Plain Text:", xor_block.hex())
+    
+    return
+
+def test(cipher, key, iv, index):
+    print("=====================RANDOM ACCESS==========================")
+    AES_random_access(cipher, key, iv, index)
+    print("============================================================")
+    return
+
+def main():
+    parser = argparse.ArgumentParser(description="AES_128_CBC Encrypt/Decrypt arguments.")
+    parser.add_argument("-p",  required=True, help="The plaintext, encoded in hexadecimal format")
+    parser.add_argument("-k",  required=True, help="The key, encoded in hexadecimal format")
+    parser.add_argument("-iv", required=True, help="The IV, encoded in hexadecimal format")
+
+    args = parser.parse_args()
+    
+    plaintext = args.p
+    key = args.k
+    iv = args.iv
+
+    #check if key and iv are 16 bytes
+    if len(key) != 32:
+        raise argparse.ArgumentTypeError(f"Invalid key length: {len(key)}")
+    if len(iv) != 32:
+        raise argparse.ArgumentTypeError(f"Invalid iv length: {len(iv)}")
+    
+
+    bplaintext = parse_hexadecimal(plaintext)
+    if len(bplaintext) <= 80:
+        raise argparse.ArgumentTypeError(f"Invalid plaintext length (lower than 6 blocks): {len(bplaintext)}")
+    
+    bkey = parse_hexadecimal(key)
+    biv = parse_hexadecimal(iv)
+    cipher = AES_128_CBC_encrypt(bplaintext, bkey, biv)
+    test(cipher, bkey, biv, 3)
+    test(cipher, bkey, biv, 1)
+    test(cipher, bkey, biv, 0)
+    return
+
+# a good run test to check python3 AES_128_random_acces.py -p ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa111111111111111111111111111111112222222222222222222222222222222233333333333333333333333333333333 -k 199494cdea9c646e76015c5bd3ffdaec -iv 199494cdea9c646e76015c5bd3ffdaec
+if __name__ == "__main__":
+    main()
